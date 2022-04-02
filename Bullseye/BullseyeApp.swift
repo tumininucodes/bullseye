8bd4a4cf112b9b38234c9cf1daf4aa17cd8e5685
Tumi <oluwatumininuojo25@gmail.com>
2022-04-02 15:35:17 +0100

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Oluwatumininu Ojo on 02/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
