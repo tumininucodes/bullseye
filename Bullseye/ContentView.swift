5b4c9419b880f1b141ee5acf90bc4c4ee424a533
Tumi <oluwatumininuojo25@gmail.com>
2022-05-18 19:04:11 +0100

    updates

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -10,21 +10,29 @@ import SwiftUI
 struct ContentView: View {
     
     @State var alertIsVisible: Bool = false
+    @State var sliderValue: Double = 50.0
+    @State var target: Int = Int.random(in: 1...100)
     
     var body: some View {
         
         VStack {
             
+            Spacer()
+            
             HStack {
                 Text("Put the text as close as you can to:")
-                Text("100")
+                Text("\(self.target)")
             }
+            
+            Spacer()
             
             HStack {
                 Text("1")
-                Slider(value: .constant(10))
+                Slider(value: $sliderValue, in: 1...100)
                 Text("100")
             }
+            
+            Spacer()
             
             Button(action: {
                 print("Button pressed")
@@ -32,9 +40,52 @@ struct ContentView: View {
             }) {
                 Text("Hit me!")
             }.alert(isPresented: self.$alertIsVisible) {
-                Alert(title: Text("Hello there!"), message: Text("This is my first pop-up."), dismissButton: .default(Text("Awesome")))
+                Alert(
+                    title: Text("Hello there!"),
+                    message: Text("The slider's value is \(Int(self.sliderValue.rounded())).\n" +
+                                  "The target value is \(self.target).\n" +
+                                  "You scored \(self.pointsForCurrentRound()) points this round."),
+                    dismissButton: .default(Text("Awesome")))
             }
+            
+            Spacer()
+            
+            HStack {
+                
+                Button(action: {}) {
+                    Text("Start over")
+                }
+                
+                Spacer()
+                
+                Text("Score:")
+                Text("99999")
+                
+                Spacer()
+                
+                Text("Round:")
+                Text("99999")
+                
+                Button(action: {}) {
+                    Text("Info")
+                }
+                
+            }
+            .padding(.bottom, 20)
         }
         
     }
+    
+    func pointsForCurrentRound() -> Int {
+      var difference: Int
+      if Int(self.sliderValue.rounded()) > self.target {
+        difference = Int(self.sliderValue.rounded()) - self.target
+      } else if self.target > Int(self.sliderValue.rounded()) {
+        difference = self.target - Int(self.sliderValue.rounded())
+      } else {
+        difference = 0
+      }
+      return 100 - difference
+    }
+    
 }
